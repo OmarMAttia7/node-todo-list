d211257eba64784cd2c4e99f3566a670c59b46b4
Omar Attia <omarmattia97@gmail.com>
2023-03-25 05:58:07 +0200

    refactor: serverHTML

--- src/lib/serveHTML/index.ts
+++ src/lib/serveHTML/index.ts
@@ -1,4 +1,4 @@
-import loadFile from '../loadFile.js';
+import loadFile from './../loadFile.js';
 import { type Link, HTMLPage } from './PageBuilder.js';
 
 interface ServeHtmlOptions {
@@ -7,16 +7,17 @@ interface ServeHtmlOptions {
   links?: Link[];
 }
 
-export default async function serveHtml({
+export async function serveHtml({
   title,
   page,
   links = [],
 }: ServeHtmlOptions): Promise<string> {
-  let content: string = 'Error 404: Not found';
+  let content: string;
   try {
     content = (await loadFile(`client/pages/${page}.html`)).toString();
   } catch (e) {
     console.log(e);
+    content = 'Error 404: Not found';
   }
 
   const htmlPage = new HTMLPage({
